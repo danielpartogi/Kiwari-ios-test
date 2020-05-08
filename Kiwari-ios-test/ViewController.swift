c68fd88f325c54a09fcd303508b75c071bcf1ee1
daniel <danielpartogi1996@gmail.com>
2020-05-08 13:31:53 +0700

    Initial Commit

---
+++ Kiwari-ios-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kiwari-ios-test
+//
+//  Created by daniel on 08/05/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Kiwari-ios-testTests/Kiwari_ios_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kiwari_ios_testTests.swift
+//  Kiwari-ios-testTests
+//
+//  Created by daniel on 08/05/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import XCTest
+@testable import Kiwari_ios_test
+
+class Kiwari_ios_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
